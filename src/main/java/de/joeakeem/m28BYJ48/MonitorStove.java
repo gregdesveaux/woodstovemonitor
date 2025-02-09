a8724a56daa4d6561fc3fc147d828a05504e091a
gdesveau <greg.desveaux@gmail.com>
2025-02-09 14:23:25 -0400

    two burn threads

--- src/main/java/de/joeakeem/m28BYJ48/MonitorStove.java
+++ src/main/java/de/joeakeem/m28BYJ48/MonitorStove.java
@@ -11,7 +11,8 @@ public class MonitorStove {
     Temperature temperature;
     int damperPosition = 1000;
     StepperMotor28BYJ48 stepperMotor = null;
-
+    int temp=0;
+    boolean inBurnLoop = false;
     public static void main(String[] args) {
 
         new MonitorStove();
@@ -32,58 +33,20 @@ public class MonitorStove {
         //stepperMotor.performDemo(rotations);
         temperature = new Temperature();
         new WebInterface(this);
-        boolean inBurnLoop = false;
-        int temp = temperature.getTemp();
+
+         temp = temperature.getTemp();
         System.out.println("Temp: " + temp);
         System.out.println("Moving damper to open");
         stepperMotor.moveDamper(damperPosition, 0);
         emergenyCloseThread();
-
-        while (true) {
-            temp = temperature.getTemp();
-            System.out.println("Temp: " + temp);
-            System.out.println("Damper: " + damperPosition);
-            if (temp > 180 && !inBurnLoop) {
-                int steps = 800;
-                int direction = 1;
-                if (steps < 0) {
-                    steps = 0 - steps;
-                    direction = 0;
-                }
-
-                stepperMotor.moveDamper(steps, direction);
-                inBurnLoop = true;
-                damperPosition = 200;
-                System.out.println("moving damper to: " + damperPosition);
-            } else if (temp > 210 && inBurnLoop && damperPosition > 100) {
-                int steps = 100;
-                int direction = 1;
-
-                stepperMotor.moveDamper(steps, direction);
-                damperPosition = damperPosition - steps;
-                System.out.println("moving damper to: " + damperPosition);
-            } else if (temp < 190 && inBurnLoop && damperPosition < 1000) {
-                int steps = 100;
-                int direction = 0;
-
-                stepperMotor.moveDamper(steps, direction);
-                damperPosition = damperPosition + steps;
-                System.out.println("moving damper to: " + damperPosition);
-            } else if (temp < 150 && inBurnLoop && damperPosition > 500) {
-                int steps = damperPosition;
-                int direction = 1;
-
-                stepperMotor.moveDamper(steps, direction);
-                inBurnLoop = false;
-                damperPosition = 0;
-                System.out.println("Fire is out, moving damper to: " + damperPosition);
-            }
-            try {
-                Thread.sleep(1000 * 60 * 5);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        hotThread();
+        try {
+            Thread.sleep(1000 * 60 * 1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+        coldThread();
+
     }
     int getTemp(){
         return temperature.getTemp();
@@ -97,7 +60,83 @@ public class MonitorStove {
         stepperMotor.moveDamper(1000-damperPosition, 0);
         damperPosition = 1000;
     }
+    void hotThread() {
+        new Thread() {
+            public void run() {
 
+                    while (true) {
+                        temp = temperature.getTemp();
+                        System.out.println("Temp: " + temp);
+                        System.out.println("Damper: " + damperPosition);
+                        if (temp > 190 && !inBurnLoop) {
+                            int steps = 800;
+                            int direction = 1;
+                            if (steps < 0) {
+                                steps = 0 - steps;
+                                direction = 0;
+                            }
+
+                            stepperMotor.moveDamper(steps, direction);
+                            inBurnLoop = true;
+                            damperPosition = 200;
+                            System.out.println("moving damper to: " + damperPosition);
+                        } else if (temp > 210 && inBurnLoop && damperPosition > 100) {
+                            int steps = 100;
+                            int direction = 1;
+
+                            stepperMotor.moveDamper(steps, direction);
+                            damperPosition = damperPosition - steps;
+                            System.out.println("moving damper to: " + damperPosition);
+                        }
+                        try {
+                            Thread.sleep(1000 * 60 * 5);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+            }
+        }.start();
+    }
+
+    void coldThread() {
+        new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(1000 * 60 * 20);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                while (true) {
+                    temp = temperature.getTemp();
+                    System.out.println("Temp: " + temp);
+                    System.out.println("Damper: " + damperPosition);
+                 if (temp < 190 && inBurnLoop && damperPosition < 1000) {
+                        int steps = 100;
+                        int direction = 0;
+
+                        stepperMotor.moveDamper(steps, direction);
+                        damperPosition = damperPosition + steps;
+                        System.out.println("moving damper to: " + damperPosition);
+                    } else if (temp < 150 && inBurnLoop && damperPosition > 500) {
+                        int steps = damperPosition;
+                        int direction = 1;
+
+                        stepperMotor.moveDamper(steps, direction);
+                        inBurnLoop = false;
+                        damperPosition = 0;
+                        System.out.println("Fire is out, moving damper to: " + damperPosition);
+                    }
+                    try {
+                        Thread.sleep(1000 * 60 * 20);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            }
+        }.start();
+    }
     void emergenyCloseThread() {
         new Thread() {
             public void run() {
